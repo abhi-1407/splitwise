2e2734eb02cb16deadcfb07c47842bbe786c0d6c
Abhilash Jena <ajena_be20@thapar.edu>
2026-06-28 17:43:07 +0530

    Adding JPA relationshipd in balance entity

--- src/main/java/com/abhilash/splitwise/entity/Balance.java
+++ src/main/java/com/abhilash/splitwise/entity/Balance.java
@@ -1,22 +1,33 @@
 package com.abhilash.splitwise.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Table(name="balances")
+@Table(name = "balances")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Balance {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "creditor_id")
     private User creditor;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "debtor_id")
     private User debtor;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id")
     private Group group;
     private long amount;
 
